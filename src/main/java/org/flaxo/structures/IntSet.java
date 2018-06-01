c0b356dcd43e29d363f83b3740669807785e5ea8
243897 <38332876+243897@users.noreply.github.com>
2018-06-01 20:15:23 +0300

    Add files via upload

--- src/main/java/org/flaxo/structures/IntSet.java
+++ src/main/java/org/flaxo/structures/IntSet.java
@@ -1,142 +1,45 @@
-package org.flaxo.structures;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * Структура данных - множество неотрицательных целых чисел.
- */
 public class IntSet {
+    private List<Integer> list = new ArrayList<>();
 
-    private IntSet() {}
-
-    /**
-     * Возвращает пустое множество.
-     *
-     * @return Пустое множество.
-     */
-    public static IntSet empty() {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
+    public void add(int element) {
+        if (element < 0) return;
+        list.add(element);
     }
 
-    /**
-     * Возвращает множество переданных чисел.
-     *
-     * @param values Числа, из которых будет состоять множество.
-     * @return Множество переданных чисел.
-     */
-    public static IntSet of(final int... values) {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
+    public void remove(int element) {
+        if (element < 0) return;
+        list.remove((Integer) element);
     }
 
-    /**
-     * Добавляет число ко множеству.
-     *
-     * @param value Число, которое необходимо добавить во множество.
-     */
-    public void add(final int value) {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
+    public boolean contains(int element) {
+        return list.contains((Integer) element);
     }
 
-    /**
-     * Удаляет число из множества.
-     *
-     * @param value Число, которое необходимо удалить из множества.
-     */
-    public void remove(final int value) {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
-    }
-
-    /**
-     * Проверяет, содержится ли значение во множестве.
-     *
-     * @param value Число, наличие которого во множестве необходимо проверить.
-     * @return true если множество содержит значение, иначе - false.
-     */
-    public boolean contains(final int value) {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
-    }
-
-    /**
-     * Возвращает размер множества.
-     *
-     * @return Размер множества.
-     */
     public int size() {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
+        return list.size();
     }
 
-    /**
-     * Возвращает множество всех элементов текущего
-     * и переданного множеств.
-     *
-     * Операция объединения множеств.
-     *
-     * @param other Множество, с которым необходимо провести операцию объединения.
-     * @return Множество, являющееся результатом объединения двух множеств.
-     */
-    public IntSet union(final IntSet other) {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
+    public boolean isSubsetOf(IntSet another) {
+        return list.containsAll(another.list);
     }
 
-    /**
-     * Возвращает множество общих элементов текущего
-     * и переданного множеств.
-     *
-     * Операция пересечения множеств.
-     *
-     * @param other Множество, с которым необходимо провести операцию пересечения.
-     * @return Множество, являющееся результатом пересечения двух множеств.
-     */
-    public IntSet intersection(final IntSet other) {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
+    public void union(IntSet another) {
+        list.addAll(another.list);
     }
 
-    /**
-     * Возвращает множество уникальных элементов текущего
-     * и переданного множеств.
-     *
-     * Операция исключающего ИЛИ над множествами.
-     *
-     * @param other Множество, с которым необходимо провести данную операцию.
-     * @return Множество, элементы содержащиеся либо только в первом, либо
-     * только во втором множестве.
-     */
-    public IntSet difference(final IntSet other) {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
+    public void intersection(IntSet another) {
+        list.retainAll(another.list);
     }
 
-    /**
-     * Возвращает множество элементов текущего множества,
-     * которых нет в переданном множестве.
-     *
-     * Операция вычитания множеств.
-     *
-     * @param other Множество, которое необходимо вычесть из текущего.
-     * @return Множество, являющееся результатом вычитания двух множеств.
-     */
-    public IntSet minus(final IntSet other) {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
+    public void minus(IntSet another) {
+        list.removeAll(another.list);
     }
 
-    /**
-     * Проверяет, является ли текущее множество подмножеством переданного.
-     *
-     * @param other Множество, для которого необходимо проверить, входит ли в
-     *              него текущее множество
-     * @return true, если текущее множество, является подмножеством переданного,
-     * иначе - false
-     */
-    public boolean isSubsetOf(final IntSet other) {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
+    public void difference(IntSet another) {
+        list.removeAll(another.list);
     }
-
 }

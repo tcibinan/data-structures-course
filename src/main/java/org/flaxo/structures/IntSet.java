9732373523b28ea2b1a1d0886af41413572aed8f
AlexeyKolokoltsev <kolokolcev.94@mail.ru>
2018-06-01 17:13:00 +0300

    Task was done

--- src/main/java/org/flaxo/structures/IntSet.java
+++ src/main/java/org/flaxo/structures/IntSet.java
@@ -1,142 +1,88 @@
 package org.flaxo.structures;
+import java.util.HashSet;
+import java.util.Set;
 
-/**
- * Структура данных - множество неотрицательных целых чисел.
- */
 public class IntSet {
 
-    private IntSet() {}
+    private Set<Integer> hs;
 
-    /**
-     * Возвращает пустое множество.
-     *
-     * @return Пустое множество.
-     */
+    private IntSet() {
+        this.hs = new HashSet<>();
+    }
+
+
     public static IntSet empty() {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
+        return new IntSet();
     }
 
-    /**
-     * Возвращает множество переданных чисел.
-     *
-     * @param values Числа, из которых будет состоять множество.
-     * @return Множество переданных чисел.
-     */
+
     public static IntSet of(final int... values) {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
+        IntSet InSet = new IntSet();
+        for (int i : values) {
+            InSet.hs.add(i);
+        }
+        return InSet;
     }
 
-    /**
-     * Добавляет число ко множеству.
-     *
-     * @param value Число, которое необходимо добавить во множество.
-     */
+
     public void add(final int value) {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
+        this.hs.add(value);
     }
 
-    /**
-     * Удаляет число из множества.
-     *
-     * @param value Число, которое необходимо удалить из множества.
-     */
+
     public void remove(final int value) {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
+        this.hs.remove(value);
     }
 
-    /**
-     * Проверяет, содержится ли значение во множестве.
-     *
-     * @param value Число, наличие которого во множестве необходимо проверить.
-     * @return true если множество содержит значение, иначе - false.
-     */
+
     public boolean contains(final int value) {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
+        return this.hs.contains(value);
     }
 
-    /**
-     * Возвращает размер множества.
-     *
-     * @return Размер множества.
-     */
+
     public int size() {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
+        return this.hs.size();
     }
 
-    /**
-     * Возвращает множество всех элементов текущего
-     * и переданного множеств.
-     *
-     * Операция объединения множеств.
-     *
-     * @param other Множество, с которым необходимо провести операцию объединения.
-     * @return Множество, являющееся результатом объединения двух множеств.
-     */
+
     public IntSet union(final IntSet other) {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
+        IntSet InSet = new IntSet();
+        InSet.hs.addAll(this.hs);
+        InSet.hs.addAll(other.hs);
+        return InSet;
     }
 
-    /**
-     * Возвращает множество общих элементов текущего
-     * и переданного множеств.
-     *
-     * Операция пересечения множеств.
-     *
-     * @param other Множество, с которым необходимо провести операцию пересечения.
-     * @return Множество, являющееся результатом пересечения двух множеств.
-     */
+
     public IntSet intersection(final IntSet other) {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
+        IntSet InSet = new IntSet();
+
+        InSet.hs.addAll(this.hs);
+        InSet.hs.addAll(other.hs);
+
+        InSet.hs.retainAll(this.hs);
+        InSet.hs.retainAll(other.hs);
+        return InSet;
     }
 
-    /**
-     * Возвращает множество уникальных элементов текущего
-     * и переданного множеств.
-     *
-     * Операция исключающего ИЛИ над множествами.
-     *
-     * @param other Множество, с которым необходимо провести данную операцию.
-     * @return Множество, элементы содержащиеся либо только в первом, либо
-     * только во втором множестве.
-     */
+
     public IntSet difference(final IntSet other) {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
+        IntSet Set = this.union(other);
+        IntSet SecondSet = this.intersection(other);
+        return Set.minus(SecondSet);
     }
 
-    /**
-     * Возвращает множество элементов текущего множества,
-     * которых нет в переданном множестве.
-     *
-     * Операция вычитания множеств.
-     *
-     * @param other Множество, которое необходимо вычесть из текущего.
-     * @return Множество, являющееся результатом вычитания двух множеств.
-     */
+
     public IntSet minus(final IntSet other) {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
+        IntSet Set = this.union(other);
+        IntSet SecondSet = this.intersection(other);
+
+        Set.hs.removeAll(SecondSet.hs);
+
+        return Set;
     }
 
-    /**
-     * Проверяет, является ли текущее множество подмножеством переданного.
-     *
-     * @param other Множество, для которого необходимо проверить, входит ли в
-     *              него текущее множество
-     * @return true, если текущее множество, является подмножеством переданного,
-     * иначе - false
-     */
+
     public boolean isSubsetOf(final IntSet other) {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
+        return other.hs.containsAll(this.hs);
     }
-
 }

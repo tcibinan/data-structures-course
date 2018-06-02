a5515af9e52a00fb1eb00d8ab7e5842cc8f30dca
vasyaprt <2sa1liut>
2018-06-02 05:52:25 +0300

    task 2

--- src/main/java/org/flaxo/structures/IntSet.java
+++ src/main/java/org/flaxo/structures/IntSet.java
@@ -1,142 +1,86 @@
 package org.flaxo.structures;
 
-/**
- * Структура данных - множество неотрицательных целых чисел.
- */
+import java.util.Set;
+import java.util.HashSet;
+
 public class IntSet {
+    private Set<Integer> set = new HashSet<>();
 
-    private IntSet() {}
+    private IntSet() {
+    }
 
-    /**
-     * Возвращает пустое множество.
-     *
-     * @return Пустое множество.
-     */
     public static IntSet empty() {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
+        return new IntSet();
+
     }
 
-    /**
-     * Возвращает множество переданных чисел.
-     *
-     * @param values Числа, из которых будет состоять множество.
-     * @return Множество переданных чисел.
-     */
     public static IntSet of(final int... values) {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
+        IntSet intset = new IntSet();
+        for (int i : values) {
+            intset.set.add(i);
+        }
+        return intset;
     }
-
-    /**
-     * Добавляет число ко множеству.
-     *
-     * @param value Число, которое необходимо добавить во множество.
-     */
     public void add(final int value) {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
+            this.set.add(value);
     }
-
-    /**
-     * Удаляет число из множества.
-     *
-     * @param value Число, которое необходимо удалить из множества.
-     */
     public void remove(final int value) {
-        // todo: Необходимо добавить реализацию метода
+        this.set.remove(value);
         throw new UnsupportedOperationException("Method is not implemented yet");
     }
 
-    /**
-     * Проверяет, содержится ли значение во множестве.
-     *
-     * @param value Число, наличие которого во множестве необходимо проверить.
-     * @return true если множество содержит значение, иначе - false.
-     */
+
     public boolean contains(final int value) {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
+        return this.set.contains(value);
+
     }
 
-    /**
-     * Возвращает размер множества.
-     *
-     * @return Размер множества.
-     */
+
     public int size() {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
+        return this.set.size();
     }
 
-    /**
-     * Возвращает множество всех элементов текущего
-     * и переданного множеств.
-     *
-     * Операция объединения множеств.
-     *
-     * @param other Множество, с которым необходимо провести операцию объединения.
-     * @return Множество, являющееся результатом объединения двух множеств.
-     */
     public IntSet union(final IntSet other) {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
+        IntSet intset = new IntSet();
+        intset.set.addAll(this.set);
+        intset.set.addAll(other.set);
+        return intset;
     }
 
-    /**
-     * Возвращает множество общих элементов текущего
-     * и переданного множеств.
-     *
-     * Операция пересечения множеств.
-     *
-     * @param other Множество, с которым необходимо провести операцию пересечения.
-     * @return Множество, являющееся результатом пересечения двух множеств.
-     */
+
     public IntSet intersection(final IntSet other) {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
+        IntSet intset = new IntSet();
+        intset.set.addAll(this.set);
+        intset.set.addAll(other.set);
+        intset.set.retainAll(this.set);
+        intset.set.retainAll(other.set);
+        return intset;
     }
 
-    /**
-     * Возвращает множество уникальных элементов текущего
-     * и переданного множеств.
-     *
-     * Операция исключающего ИЛИ над множествами.
-     *
-     * @param other Множество, с которым необходимо провести данную операцию.
-     * @return Множество, элементы содержащиеся либо только в первом, либо
-     * только во втором множестве.
-     */
-    public IntSet difference(final IntSet other) {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
+   IntSet difference(final IntSet other) {
+        IntSet intset = this.union(other);
+        IntSet intset2 = this.intersection(other);
+        return intset.minus(intset2);
     }
 
-    /**
-     * Возвращает множество элементов текущего множества,
-     * которых нет в переданном множестве.
-     *
-     * Операция вычитания множеств.
-     *
-     * @param other Множество, которое необходимо вычесть из текущего.
-     * @return Множество, являющееся результатом вычитания двух множеств.
-     */
+
+
     public IntSet minus(final IntSet other) {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
+        IntSet intset = this.union(other);
+        IntSet intset2 = this.intersection(other);
+        intset.set.removeAll(intset2.set);
+        return intset;
     }
 
-    /**
-     * Проверяет, является ли текущее множество подмножеством переданного.
-     *
-     * @param other Множество, для которого необходимо проверить, входит ли в
-     *              него текущее множество
-     * @return true, если текущее множество, является подмножеством переданного,
-     * иначе - false
-     */
+
     public boolean isSubsetOf(final IntSet other) {
-        // todo: Необходимо добавить реализацию метода
-        throw new UnsupportedOperationException("Method is not implemented yet");
+        if(this.intersection(other).set.size() == 0){
+            return false;
+        }
+        else if(other.minus(this).set.size() == other.set.size() - this.set.size()){
+            return true;
+        }
+        return false;
     }
 
 }

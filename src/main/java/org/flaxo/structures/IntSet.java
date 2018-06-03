b31f51649fe99d1e3d715947495143b489dd9338
vasyaprt <2sa1liut>
2018-06-03 15:28:09 +0300

    task-2

--- src/main/java/org/flaxo/structures/IntSet.java
+++ src/main/java/org/flaxo/structures/IntSet.java
@@ -4,10 +4,10 @@ import java.util.Set;
 import java.util.HashSet;
 
 public class IntSet {
-    private Set<Integer> set = new HashSet<>();
-
+    private Set<Integer> hashSet;
     private IntSet() {
-    }
+                this.hashSet = new HashSet<>();
+        }
 
     public static IntSet empty() {
         return new IntSet();
@@ -17,47 +17,45 @@ public class IntSet {
     public static IntSet of(final int... values) {
         IntSet intset = new IntSet();
         for (int i : values) {
-            intset.set.add(i);
+            intset.hashSet.add(i);
         }
         return intset;
     }
     public void add(final int value) {
-            this.set.add(value);
+            this.hashSet.add(value);
     }
     public void remove(final int value) {
-        this.set.remove(value);
-        throw new UnsupportedOperationException("Method is not implemented yet");
+        this.hashSet.remove(value);
     }
 
 
     public boolean contains(final int value) {
-        return this.set.contains(value);
+        return this.hashSet.contains(value);
 
     }
 
-
     public int size() {
-        return this.set.size();
+        return this.hashSet.size();
     }
 
     public IntSet union(final IntSet other) {
         IntSet intset = new IntSet();
-        intset.set.addAll(this.set);
-        intset.set.addAll(other.set);
+        intset.hashSet.addAll(this.hashSet);
+        intset.hashSet.addAll(other.hashSet);
         return intset;
     }
 
 
     public IntSet intersection(final IntSet other) {
         IntSet intset = new IntSet();
-        intset.set.addAll(this.set);
-        intset.set.addAll(other.set);
-        intset.set.retainAll(this.set);
-        intset.set.retainAll(other.set);
+        intset.hashSet.addAll(this.hashSet);
+        intset.hashSet.addAll(other.hashSet);
+        intset.hashSet.retainAll(this.hashSet);
+        intset.hashSet.retainAll(other.hashSet);
         return intset;
     }
 
-   IntSet difference(final IntSet other) {
+   public IntSet difference(final IntSet other) {
         IntSet intset = this.union(other);
         IntSet intset2 = this.intersection(other);
         return intset.minus(intset2);
@@ -68,16 +66,16 @@ public class IntSet {
     public IntSet minus(final IntSet other) {
         IntSet intset = this.union(other);
         IntSet intset2 = this.intersection(other);
-        intset.set.removeAll(intset2.set);
+        intset.hashSet.removeAll(intset2.hashSet);
         return intset;
     }
 
 
     public boolean isSubsetOf(final IntSet other) {
-        if(this.intersection(other).set.size() == 0){
+        if(this.intersection(other).hashSet.size() == 0){
             return false;
         }
-        else if(other.minus(this).set.size() == other.set.size() - this.set.size()){
+        else if(other.minus(this).hashSet.size() == other.hashSet.size() - this.hashSet.size()){
             return true;
         }
         return false;
